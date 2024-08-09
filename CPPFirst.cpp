233571393f18c417cde28af4cf87c0c39d74334e
Riya UT <156648774+riya-ut@users.noreply.github.com>
2024-08-09 20:44:20 +0530

    Add files via upload

---
+++ CPPFirst.cpp
@@ -0,0 +1,15 @@
+/*
+Author:     Riya Arjun
+Topic:      Getting started with C++
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    // My first print statement while learning C++;
+    cout << "Hello Arjun." << endl << "I love you.\n";
+    cout << "Hello Arjun. \nI love you.";
+
+    return 0;
+
+}
